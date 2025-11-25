20ce7e9ec66aede762afa40d36e0e6b3979e673d
Arin7712 <aringawande7712@gmail.com>
2025-11-25 11:19:57 +0530

    fix

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { redirect } from "next/navigation";
 
