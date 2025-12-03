e0bbc546fe8d90d38df7d5b3b30385b83f7659f9
Arin7712 <aringawande7712@gmail.com>
2025-12-03 12:50:42 +0530

    view

---
+++ app/view/page.tsx
@@ -0,0 +1,12 @@
+import PaymentImage from '@/components/PaymentImage'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <PaymentImage/>
+    </div>
+  )
+}
+
+export default page
